5548e3eef5babd54de09efda2b149dca88834854
Acsah Rojan <acsah.rojan@gmail.com>
2025-04-12 01:10:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './components/Home'
+import Dashboard from './components/Dashboard'
+import BlogForm from './components/BlogForm'
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{textAlign:"center"}}>BLOG APPLICATION</h1>
+      <Router>
+        <div className='App'>
+          <Header/>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/blogform' element={<BlogForm />} />      
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  )
+}
+
+export default App
